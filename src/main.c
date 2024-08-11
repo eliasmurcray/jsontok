a9c78245901c2890e8f00bf6d4d62de47fca316d
eliasmurcray <eliasmurcray@gmail.com>
2024-08-11 14:32:02 -0700

    Add parsing speed tests

--- src/main.c
+++ src/main.c
@@ -1,33 +1,50 @@
 #include "jsontok.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <inttypes.h>
 
-int main() {
-  const char* my_json = "{\"foo\":\"bar\",\"baz\":1,\"nest\":{\"pi\":3.41}}";
-  printf("Input: %s\n", my_json);
+const char *read_file(const char *path) {
+  FILE *file = fopen(path, "r");
+  if (!file) return NULL;
+  fseek(file, 0, SEEK_END);
+  long len = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char *buffer = malloc(len + 1);
+  if (!buffer) {
+    fclose(file);
+    return NULL;
+  }
+  fread(buffer, 1, len, file);
+  buffer[len] = '\0';
+  return buffer;
+}
+
+void test_parse(const char *path) {
+  printf("Running %s parse test...\n", path);
+  const char *json = read_file(path);
+  if (json == NULL) {
+    fprintf(stderr, "Failed to get %s\n", path);
+    return;
+  }
+  struct timespec end, start;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
   enum JsonError error;
-  struct JsonToken *token = jsontok_parse(my_json, &error);
-  if (!token) {
-    fprintf(stderr, "Error parsing JSON: %s\n", jsontok_strerror(error));
-    return 1;
-  }
-  struct JsonToken *foo = jsontok_get(token->as_object, "foo");
-  if (foo && foo->type == JSON_STRING) {
-    printf("value of foo is %s\n", foo->as_string);
-  }
-  struct JsonToken *bar = jsontok_get(token->as_object, "baz");
-  if (bar && bar->type == JSON_LONG) {
-    printf("value of bar is %ld\n", bar->as_long);
-  }
-  struct JsonToken *nest = jsontok_get(token->as_object, "nest");
-  if (nest && nest->type == JSON_WRAPPED_OBJECT) {
-    struct JsonToken *obj = jsontok_unwrap(nest, &error);
-    if (obj && obj->type == JSON_OBJECT) {
-      struct JsonToken *pi = jsontok_get(obj->as_object, "pi");
-      if (pi && pi->type == JSON_DOUBLE) {
-        printf("value of nest.pi is %f\n", pi->as_double);
-      }
-    }
-    jsontok_free(obj);
+  struct JsonToken *token = jsontok_parse(json, &error);
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+  if (token == NULL) {
+    fprintf(stderr, "Failed to parse JSON: %s\n", jsontok_strerror(error));
+    return;
   }
   jsontok_free(token);
+
+  uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+  printf("Successfully parsed %s (%zu bytes) in %" PRIu64 "us\n\n", path, strlen(json), delta_us);
+}
+
+int main() {
+  test_parse("./samples/simple.json");
+  test_parse("./samples/food.json");
+  test_parse("./samples/reddit.json");
 }
